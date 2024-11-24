f59548771ec7c561929b05404b5a0ebc50fe266d
GOATAFI <projectexecutiveofpm@gmail.com>
2024-11-24 22:48:29 +0600

    data dekhayte partisi

--- hub.js
+++ hub.js
@@ -4,6 +4,20 @@ const loadAiData = async () => {
   //   console.log(data.data.tools);
   const aiData = data.data.tools;
   //   console.log(aiData);
+  displayData(aiData);
+};
+
+const displayData = (aiData) => {
+  console.log(aiData);
+  const showAiDataDiv = document.getElementById("show-ai-data");
+  for (const singleData of aiData) {
+    console.log(singleData);
+    const dataCard = document.createElement("div");
+    dataCard.innerHTML = `
+    <h1>${singleData.description}</h1>
+    `;
+    showAiDataDiv.appendChild(dataCard);
+  }
 };
 
 loadAiData();
